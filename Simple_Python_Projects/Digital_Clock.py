11ef9a6092ccc4f495eeccaa2c694c17d4c24052
privateboss1 <89046656+privateboss0@users.noreply.github.com>
2023-10-21 21:16:24 +0100

    Add files via upload

---
+++ Simple_Python_Projects/Digital_Clock.py
@@ -0,0 +1,23 @@
+from tkinter import *
+import time
+
+def main():
+  root = Tk()
+  root.title("Digital Clock")
+
+  # Create the time label.
+  time_label = Label(root, font=("TimesNewRoman", 76, "bold italic"))
+  time_label.pack()
+
+  # Update the time every second, for the 12hr(AM/PM) clock
+  def update_time():
+    am_or_pm = time.strftime("%p")
+    current_time = time.strftime("%I:%M:%S" + " " + am_or_pm)
+    time_label.config(text=current_time)
+    time_label.after(1000, update_time)
+
+  update_time()
+  root.mainloop()
+
+if __name__ == "__main__":
+  main()
