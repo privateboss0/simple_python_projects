5e7ac0a86da52e0995a0b3e30b65187305bf4751
privateboss1 <89046656+privateboss0@users.noreply.github.com>
2023-11-07 15:14:11 +0100

    Digital_Clock.py

--- Simple_Python_Projects/Digital_Clock.py
+++ Simple_Python_Projects/Digital_Clock.py
@@ -3,7 +3,7 @@ import time
 
 def main():
   root = Tk()
-  root.title("Digital Clock")
+  root.title("Digital Clock with Date")
 
   # Create the time label. winks!
   time_label = Label(root, font=("TimesNewRoman", 76, "bold italic"))
@@ -13,7 +13,9 @@ def main():
   def update_time():
     am_or_pm = time.strftime("%p")
     current_time = time.strftime("%I:%M:%S" + " " + am_or_pm)
-    time_label.config(text=current_time)
+    current_date = time.strftime("%A, %B %d, %Y")
+
+    time_label.config(text=current_time + "\n" + current_date)
     time_label.after(1000, update_time)
 
   update_time()
