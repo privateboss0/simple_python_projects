194f9e0f1b6a6d27ab1d8819d9ac755950abeca1
privateboss1 <89046656+privateboss0@users.noreply.github.com>
2023-11-17 08:53:23 +0100

    calender_Pink.py

--- Simple_Python_Projects/calender_Pink.py
+++ Simple_Python_Projects/calender_Pink.py
@@ -9,14 +9,14 @@ current_day = datetime.date.today().day
 calendar.setfirstweekday(3)
 
 # Create regular expression patterns
-prday = r'\b{current_year}\b'.format(current_year=current_year)
-pnightr = r"\033[35m\033[1m{current_year}\033[0m".format(current_year=current_year)
-pdayr = r'\b{current_day}\b'.format(current_day=current_day)
-prnight = r"\033[35m\033[1m{current_day}\033[0m".format(current_day=current_day)
+gday = r'\b{current_year}\b'.format(current_year=current_year)
+gnight = r"\033[32m\033[1m{current_year}\033[0m".format(current_year=current_year)
+gdayg = r'\b{current_day}\b'.format(current_day=current_day)
+gnightg = r"\033[32m\033[1m{current_day}\033[0m".format(current_day=current_day)
 
-# Replace the current year and date with pink colored coloured date
+# Replace the current year and date with green colored coloured date
 cal_text = calendar.month(current_year, current_month)
-cal_text = re.sub(prday, pnightr, cal_text)
-cal_text = re.sub(pdayr, prnight, cal_text)
+cal_text = re.sub(gday, gnight, cal_text)
+cal_text = re.sub(gdayg, gnightg, cal_text)
 
 print(cal_text)
