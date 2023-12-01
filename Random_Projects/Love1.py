17aae5e0b4fbebd573d4e55687d8a2d3c273eb88
Akinkunmi Leo Ola <89046656+privateboss0@users.noreply.github.com>
2023-12-01 12:14:55 +0100

    GIL

---
+++ Random_Projects/Love1.py
@@ -0,0 +1,52 @@
+import turtle
+from turtle import *
+
+t = turtle.Turtle()
+t.speed(0)
+
+#Draw the Purple heart
+t.color("Purple")
+t.begin_fill()
+t.pensize(3)
+t.left(50)
+t.forward(133)
+t.circle(50, 200)
+t.right(140)
+t.circle(50, 200)
+t.forward(133)
+t.end_fill()
+
+t.penup()
+t.setheading(0)  # Set the turtle's heading to 0 degrees
+t.goto(-270, 0)
+t.pendown()
+
+#Draw the Red heart
+t.color("Red")
+t.begin_fill()
+t.pensize(3)
+t.left(50)
+t.forward(133)
+t.circle(50, 200)
+t.right(140)
+t.circle(50, 200)
+t.forward(133)
+t.end_fill()
+
+t.penup()
+t.setheading(0)  # Set the turtle's heading to 0 degrees
+t.goto(270, 0)
+t.pendown()
+
+#Draw the Blue heart
+t.color("Blue")
+t.begin_fill()
+t.pensize(3)
+t.left(50)
+t.forward(133)
+t.circle(50, 200)
+t.right(140)
+t.circle(50, 200)
+t.forward(133)
+t.end_fill()
+turtle.Screen().mainloop()
