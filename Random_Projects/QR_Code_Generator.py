edc00d03f4ec35498c1cc4438c3855891dfba6d4
Akinkunmi Leo Ola <89046656+privateboss0@users.noreply.github.com>
2023-12-02 21:33:47 +0100

    QR Code Generator

--- Random_Projects/QR_Code_Generator.py
+++ Random_Projects/QR_Code_Generator.py
@@ -1,6 +1,6 @@
 import qrcode
 
-# link which represents the QR code
+# link which represents the QR code to the youtube link or any website you want
 link = "https://www.youtube.com/watch?v=3ji4PcqIBEg"
 
 # Generate and save QR code
